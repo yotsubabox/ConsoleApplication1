f9997ac4f8f0e654d5fc52892b6e7c4d3e3f3817
yotsubabox <83526631+yotsubabox@users.noreply.github.com>
2022-06-27 10:23:53 +0900

    Update Program.cs
    web edit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("this is TestCode for GitHub");
+            Consoel.WriteLine("This is Edit to Chrome");
         }
     }
 }
